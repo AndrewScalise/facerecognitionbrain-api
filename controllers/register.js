01753c7a1fcdf541511edac3b7e81a7b6bbc50a7
Mugen <paradigmcomplex@gmail.com>
2022-06-15 13:13:15 -0500

    add validation for empty register form

--- controllers/register.js
+++ controllers/register.js
@@ -2,6 +2,9 @@ const saltRounds = 10;
 
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json("incorrect form submission");
+  }
   let encryptedPassword = "";
   bcrypt.genSalt(saltRounds, function (err, salt) {
     bcrypt.hash(password, salt, function (err, hash) {
