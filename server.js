76b9459654bd272503e67cf59edde63802fdd084
Mugen <paradigmcomplex@gmail.com>
2022-06-16 13:16:02 -0500

    make port dynamic with process env port

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.put("/image", (req, res) => {
   image.handleImage(req, res, db);
 });
 
-app.listen(3000, () => {
-  console.log(`Servver is listening on port 3000`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Servver is listening on port ${process.env.PORT}`);
 });
